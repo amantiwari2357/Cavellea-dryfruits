52b96dacc64b876f353d9eaf3004450cc5e67843
digiindia2025 <amantiwariat2005@gmail.com>
2025-05-31 15:11:06 +0530

    added responsiveness in search bar

--- frontend/src/components/layout/Navbar/TopNavbar/index.jsx
+++ frontend/src/components/layout/Navbar/TopNavbar/index.jsx
@@ -71,6 +71,7 @@ const data = [
 
 const TopNavbar = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [showMobileSearch, setShowMobileSearch] = useState(false);
   const menuRef = useRef(null);
 
   useEffect(() => {
@@ -84,80 +85,133 @@ const TopNavbar = () => {
   }, []);
 
   return (
-    <nav className="sticky top-0 bg-white z-20">
-      <div className="flex relative max-w-frame mx-auto items-center justify-between md:justify-start py-5 md:py-6 px-4 xl:px-0">
-        <div className="flex items-center">
-          <div className="block md:hidden mr-4">
-            <ResTopNavbar data={data} />
+    <>
+      <nav className="sticky top-0 bg-white z-20">
+        <div className="flex relative max-w-frame mx-auto items-center justify-between md:justify-start py-5 md:py-6 px-4 xl:px-0">
+          {/* Logo & Mobile Menu */}
+          <div className="flex items-center">
+            <div className="block md:hidden mr-4">
+              <ResTopNavbar data={data} />
+            </div>
+            <Link
+              href="/"
+              className={cn([integralCF.className, "text-2xl lg:text-[32px] mb-2 mr-3 lg:mr-10"])}
+            >
+              <Image
+                src="/images/logo.jpg"
+                alt="Logo"
+                width={120}
+                height={40}
+                className="h-auto w-auto md:w-[180px] md:h-[50px]"
+                priority
+              />
+            </Link>
           </div>
-          <Link href="/" className={cn([integralCF.className, "text-2xl lg:text-[32px] mb-2 mr-3 lg:mr-10"])}>
-            <Image
-              src="/images/logo.jpg"
-              alt="Logo"
-              width={120}
-              height={40}
-              className="h-auto w-auto md:w-[180px] md:h-[50px]"
-              priority
+
+          {/* Main Navigation */}
+          <NavigationMenu className="hidden md:flex mr-2 lg:mr-7">
+            <NavigationMenuList>
+              {data.map((item) => (
+                <div key={item.id}>
+                  {item.type === "MenuItem" && <MenuItem label={item.label} url={item.url} />}
+                  {item.type === "MenuList" && <MenuList data={item.children} label={item.label} />}
+                </div>
+              ))}
+            </NavigationMenuList>
+          </NavigationMenu>
+
+          {/* Desktop Search */}
+          <InputGroup className="hidden md:flex bg-[#F0F0F0] mr-3 lg:mr-30 w-48 md:w-64 lg:w-80 px-2 py-1.5 rounded-md">
+            <InputGroup.Text className="p-1">
+              <Image
+                priority
+                src="/icons/search.svg"
+                height={16}
+                width={16}
+                alt="search"
+                className="min-w-4 min-h-4"
+              />
+            </InputGroup.Text>
+            <InputGroup.Input
+              type="search"
+              name="search"
+              placeholder="Search..."
+              className="bg-transparent placeholder:text-black/40 text-sm px-2 py-1 w-full"
             />
-          </Link>
-        </div>
+          </InputGroup>
 
-        <NavigationMenu className="hidden md:flex mr-2 lg:mr-7">
-          <NavigationMenuList>
-            {data.map((item) => (
-              <div key={item.id}>
-                {item.type === "MenuItem" && <MenuItem label={item.label} url={item.url} />}
-                {item.type === "MenuList" && <MenuList data={item.children} label={item.label} />}
-              </div>
-            ))}
-          </NavigationMenuList>
-        </NavigationMenu>
+          {/* Cart & User */}
+          <div className="flex items-center">
+            {/* Mobile Search Icon */}
+            <Link
+              href="#"
+              className="block md:hidden mr-[14px] p-1"
+              onClick={(e) => {
+                e.preventDefault();
+                setShowMobileSearch(true);
+              }}
+            >
+              <Image priority src="/icons/search-black.svg" height={22} width={22} alt="search" />
+            </Link>
 
-        <InputGroup className="hidden md:flex bg-[#F0F0F0] mr-3 lg:mr-30 w-48 md:w-64 lg:w-80 px-2 py-1.5 rounded-md">
-          <InputGroup.Text className="p-1">
-            <Image priority src="/icons/search.svg" height={16} width={16} alt="search" className="min-w-4 min-h-4" />
-          </InputGroup.Text>
-          <InputGroup.Input
-            type="search"
-            name="search"
-            placeholder="Search..."
-            className="bg-transparent placeholder:text-black/40 text-sm px-2 py-1 w-full"
-          />
-        </InputGroup>
+            <CartBtn />
 
-        <div className="flex items-center">
-          <Link href="/search" className="block md:hidden mr-[14px] p-1">
-            <Image priority src="/icons/search-black.svg" height={22} width={22} alt="search" />
-          </Link>
-          <CartBtn />
+            {/* User Profile Dropdown */}
+            <div
+              className="relative"
+              ref={menuRef}
+              onMouseEnter={() => setIsOpen(true)}
+              onMouseLeave={() => setIsOpen(false)}
+            >
+              <button className="p-1 rounded-full hover:bg-gray-200 transition">
+                <Image priority src="/icons/user.svg" height={22} width={22} alt="user" />
+              </button>
 
-          {/* User Profile Dropdown */}
-          <div
-            className="relative"
-            ref={menuRef}
-            onMouseEnter={() => setIsOpen(true)}
-            onMouseLeave={() => setIsOpen(false)}
-          >
-            <button className="p-1 rounded-full hover:bg-gray-200 transition">
-              <Image priority src="/icons/user.svg" height={22} width={22} alt="user" />
-            </button>
-
-            {isOpen && (
-              <div className="absolute right-0 mt-2 w-48 bg-white shadow-lg rounded-lg overflow-hidden border border-gray-200">
-                <ul className="flex flex-col text-gray-900">
-                  <li><Link href="/profile" className="block px-4 py-2 hover:bg-gray-200">My Profile</Link></li>
-                  <li><Link href="/orders" className="block px-4 py-2 hover:bg-gray-200">Orders</Link></li>
-                  <li><Link href="/coupons" className="block px-4 py-2 hover:bg-gray-200">Coupons</Link></li>
-                  <li><Link href="/gift-cards" className="block px-4 py-2 hover:bg-gray-200">Gift Cards</Link></li>
-                  <li><Link href="/notifications" className="block px-4 py-2 hover:bg-gray-200">Notifications</Link></li>
-                  <li><Link href="/signin" className="block px-4 py-2 text-red-600 hover:bg-gray-200">Sign Out</Link></li>
-                </ul>
-              </div>
-            )}
+              {isOpen && (
+                <div className="absolute right-0 mt-2 w-48 bg-white shadow-lg rounded-lg overflow-hidden border border-gray-200">
+                  <ul className="flex flex-col text-gray-900">
+                    <li><Link href="/profile" className="block px-4 py-2 hover:bg-gray-200">My Profile</Link></li>
+                    <li><Link href="/orders" className="block px-4 py-2 hover:bg-gray-200">Orders</Link></li>
+                    <li><Link href="/coupons" className="block px-4 py-2 hover:bg-gray-200">Coupons</Link></li>
+                    <li><Link href="/gift-cards" className="block px-4 py-2 hover:bg-gray-200">Gift Cards</Link></li>
+                    <li><Link href="/notifications" className="block px-4 py-2 hover:bg-gray-200">Notifications</Link></li>
+                    <li><Link href="/signin" className="block px-4 py-2 text-red-600 hover:bg-gray-200">Sign Out</Link></li>
+                  </ul>
+                </div>
+              )}
+            </div>
           </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+
+      {/* Mobile Search Overlay */}
+      {showMobileSearch && (
+        <div className="absolute top-0 left-0 w-full h-full bg-white z-50 flex items-center justify-between px-4">
+          <div className="flex-grow flex items-center bg-[#F0F0F0] px-3 py-2 rounded-md mr-2">
+            <Image
+              priority
+              src="/icons/search.svg"
+              height={16}
+              width={16}
+              alt="search"
+              className="min-w-4 min-h-4"
+            />
+            <input
+              type="text"
+              placeholder="Search..."
+              className="bg-transparent placeholder:text-black/40 text-sm px-2 py-1 w-full outline-none"
+              autoFocus
+            />
+          </div>
+          <button
+            onClick={() => setShowMobileSearch(false)}
+            className="p-2 text-black rounded hover:bg-gray-200 transition"
+          >
+            ✕
+          </button>
+        </div>
+      )}
+    </>
   );
 };
 
